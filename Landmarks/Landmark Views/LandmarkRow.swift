72faf809c5a5dc7c7192a3b5ad322d7f5cd8fee9
Ryan Allen <rallen3882@gmail.com>
2020-03-29 08:55:55 -0400

    Add a bunch of image/JSON resources from the project files. Also created the LandmarkRow view.

---
+++ Landmarks/Landmark Views/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ryan Allen on 3/29/20.
+//  Copyright © 2020 Ryan Allen. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    // "stored properties"
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            // How? Is it auto-resolving to landmark.imageName?
+            landmark.image
+                // why no auto-complete while typing these modifiers?
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        // landmarkData[0] autoloads from @/Resources/landmarkData.json ???...
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
